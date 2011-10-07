ea9e7f65dc7d86ffaa77759f51ba1bcc6fce4ea0
Sebastien Bonnegent <sebastien.bonnegent@gmail.com>
2011-10-07 16:24:43 +0200

    join differents urls.py in one

--- possum/urls.py
+++ possum/urls.py
@@ -2,17 +2,16 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^benchmark/', include('benchmark.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^base/', include('possum.base.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+)
 
+urlpatterns += patterns('possum.base.views',
+    (r'^base/', include('possum.base.urls')),
+    (r'^$', 'accueil'),
+    (r'^accueil$', 'accueil'),
+    (r'^factures$', 'factures'),
+    (r'^facture/(?P<id_facture>\d+)/$', 'facture'),
 )
 
 if settings.DEBUG:
